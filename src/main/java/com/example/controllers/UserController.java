0f8e8fdbce0ed0256ee82bcfb0a7a1346d18d03a
craxh21 <mbcraxh21@gmail.com>
2025-06-30 20:54:38 +0530

    add contact screen

--- src/main/java/com/example/controllers/UserController.java
+++ src/main/java/com/example/controllers/UserController.java
@@ -5,9 +5,12 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.dao.UserRepository;
+import com.example.entities.Contact;
 import com.example.entities.User;
 
 @Controller
@@ -17,8 +20,9 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
-	@RequestMapping("/index")
-	public String dashboard(Model model, Principal principal) {
+	@ModelAttribute
+	public void addCommonDaat(Model model , Principal principal){
+
 		String username = principal.getName();  //username is the email
 		System.out.println("username:" + username);
 
@@ -26,7 +30,21 @@ public class UserController {
 		// System.out.println("USER data: "+ user.toString());
 
 		model.addAttribute("user", user); //by this we can use the user object in html pages
+	}
 
+	@RequestMapping("/index")
+	public String dashboard(Model model, Principal principal) {
+		model.addAttribute("title","User dashboard");
 		return "normal/user_dashboard";
 	}
+
+	@GetMapping("/add_contact")
+	public String openAddContact(Model model){
+		model.addAttribute("title","Add Contact");
+		model.addAttribute("contact", new Contact());
+
+		return "normal/add_contact";
+	}
+
+	
 }
